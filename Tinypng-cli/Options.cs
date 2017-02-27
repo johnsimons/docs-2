2283a1c1fab10606b0ff2598a9b959fce697cd23
John Simons <john.simons@octopus.com>
2017-02-27 11:54:17 +1000

    Process images

---
+++ Tinypng-cli/Options.cs
@@ -0,0 +1,13 @@
+﻿using CommandLine;
+
+namespace Tinypng_cli
+{
+    class Options
+    {
+        [Option('p', "path", Required = true, HelpText = "Path of docs.")]
+        public string DocsPath { get; set; }
+
+        [Option('k', "key", Required = true, HelpText = "Tinify API key.")]
+        public string TinifyKey { get; set; }
+    }
+}
---
+++ Tinypng-cli/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using TinifyAPI;
+
+namespace Tinypng_cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var options = new Options();
+
+            if (!CommandLine.Parser.Default.ParseArgumentsStrict(args, options))
+            {
+                Console.WriteLine(CommandLine.Text.HelpText.AutoBuild(options));
+                return;
+            }
+
+            Run(options);
+
+            Console.Out.WriteLine("All done!");
+        }
+
+        static void Run(Options options)
+        {
+            Tinify.Key = options.TinifyKey;
+
+            var processedFiles = new string[0];
+            var location = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var processedFilesTxt = Path.Combine(location, "processed-files.txt");
+
+            if (File.Exists(processedFilesTxt))
+            {
+                processedFiles = File.ReadAllLines(processedFilesTxt);
+            }
+
+            Environment.CurrentDirectory = options.DocsPath;
+
+            var streamWriter = File.AppendText(processedFilesTxt);
+            var lockObj = new object();
+
+            var files = Directory.EnumerateFiles(".", "*.*", SearchOption.AllDirectories);
+
+            Parallel.ForEach(files, new ParallelOptions {MaxDegreeOfParallelism = 5}, file =>
+            {
+                if (!file.EndsWith(".png", true, CultureInfo.CurrentCulture) &&
+                    !file.EndsWith(".jpg", true, CultureInfo.CurrentCulture))
+                {
+                    return;
+                }
+
+                if (processedFiles.Any(s => s == file))
+                {
+                    Console.Out.WriteLine($"Skipping {file}");
+
+                    return;
+                }
+
+                Task.Run(async () =>
+                {
+                    Source source;
+                    try
+                    {
+                        source = await Tinify.FromFile(file);
+
+                    }
+                    catch (System.Exception e)
+                    {
+                        await Console.Out.WriteLineAsync($"Failed to process {file}. {e}");
+                        return;
+                    }
+
+                    if (source == null)
+                    {
+                        return;
+                    }
+
+                    await source.ToFile(file);
+                    lock (lockObj)
+                    {
+                        streamWriter.WriteLine(file);
+                        streamWriter.Flush();
+                    }
+                    await Console.Out.WriteLineAsync($"Processed {file}");
+                }).ConfigureAwait(false).GetAwaiter().GetResult();
+            });
+        }
+    }
+}
